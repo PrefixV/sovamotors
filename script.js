4454fd5cd81735bef09ec24bbe8d60fa080068f2
PrefixV <54990471+PrefixV@users.noreply.github.com>
2026-02-18 19:14:22 +0300

    Totaly changed
    styles, nav, mobile, socials

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+// ОТКРЫТИЕ МОБИЛЬНОГО МЕНЮ
+
 const navMobileButton = document.querySelector(".nav__mobile__button");
 const header = document.querySelector(".header");
 
 navMobileButton.addEventListener('click', () => {
     header.classList.toggle("header__mobile__active");
-})
+});
+
+// БЛОК СОЦИАЛОК
+
+const socialBlock = document.querySelector('.social__block');
+const triggerBtn = document.getElementById('social-trigger');
+
+triggerBtn.addEventListener('click', () => {
+  socialBlock.classList.toggle('social__block__hidden');
+});
+
+document.addEventListener('click', (e) => {
+  if (!socialBlock.contains(e.target) && !triggerBtn.contains(e.target)) {
+    socialBlock.classList.add('social__block__hidden');
+  }
+});
